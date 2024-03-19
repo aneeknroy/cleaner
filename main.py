c4b801de27c14990be1ead33256db829349858e8
Aneek Roy <hawkroy22@gmail.com>
2024-03-19 12:05:06 -0500

    AR

--- main.py
+++ main.py
@@ -41,6 +41,39 @@ def bandpass(csv_file):
         return smoothed_ppg_1
 
 
+def highpass(csv_file):
+     # Load PPG data from CSV file
+    ppg_data = pd.read_csv(csv_file)
+    noisy_ppg_1 = ppg_data["PPG1"].values
+    noisy_ppg_2 = ppg_data["PPG2"].values
+
+    # Sampling frequency (assuming it's known)
+    sampling_freq = 100  # Hz
+
+    #--------------------------------------------------------------------------------------------------------------
+    # Define Filter Parameters
+    # HIGHPASS parameters
+
+    cutoff = 0.5                         # Cutoff Frequency in Hz
+    nyquist_freq = 0.5 * sampling_freq   # Nyquist frequency in Hz
+    order = 4                            # Filter order
+
+    #--------------------------------------------------------------------------------------------------------------
+    b, a = signal.butter(order, cutoff / nyquist_freq, btype='high')
+
+    # Apply highpass filter to PPG signal
+    filtered_ppg_1 = signal.filtfilt(b, a, noisy_ppg_1)
+
+    # Baseline Correction
+    baseline_ppg_1 = signal.medfilt(filtered_ppg_1, kernel_size=51)  # Apply a median filter as a baseline
+    corrected_ppg_1 = filtered_ppg_1 - baseline_ppg_1
+
+    # Smoothing
+    smoothed_ppg_1 = signal.savgol_filter(corrected_ppg_1, window_length=51, polyorder=3)
+
+    return smoothed_ppg_1
+
+
 # Remove DC component from PPG signal
 def remove_dc_component(ppg_values):
     dc_component = np.mean(ppg_values)  # Calculate mean as DC component
@@ -58,11 +91,11 @@ def calculate_ac_component(ppg_ac):
 
 if __name__ == "__main__":
     # Load PPG data
-    bandPPG = bandpass("ppg_GREEN/green5_AR.csv")
-
+    #bandPPG = bandpass("ppg_GREEN/green5_AR.csv")
+    highPPG = highpass("ppg_GREEN/green1_AR.csv")
 
     # Remove DC component
-    ppg_ac = remove_dc_component(bandPPG)
+    ppg_ac = remove_dc_component(highPPG)
 
     # Calculate AC component
     ac_component = calculate_ac_component(ppg_ac)
