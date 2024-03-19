266e88de267a2d06c4524a64bb7cea80acd7e486
Aneek Roy <hawkroy22@gmail.com>
2024-03-19 10:49:12 -0500

    AR

--- ac.py
+++ ac.py
@@ -1,5 +1,11 @@
 import numpy as np
 import pandas as pd
+from scipy.signal import find_peaks
+
+
+
+# TODO - make this more accurate - add parameters to peak 2 peak in order stop the values getting   
+#        skewed by a very large peak therefore messing up the whole entire AC_Componenet
 
 # Load PPG data from CSV file
 def load_ppg_data(csv_file):
@@ -17,14 +23,18 @@ def remove_dc_component(ppg_values):
 
 # Calculate AC component using peak-to-peak method
 def calculate_ac_component(ppg_ac):
-    ac_component = np.ptp(ppg_ac)  # Peak-to-peak amplitude
+    peaks, _ = find_peaks(ppg_ac)  # Adjust height threshold as needed
+    troughs, _ = find_peaks(-ppg_ac)  # Detect troughs by inverting signal
+
+    ac_component = ppg_ac[peaks].mean() - ppg_ac[troughs].mean()
+    # ac_component = np.ptp(ppg_ac)  # Peak-to-peak amplitude 
     return ac_component
 
 # AC represents the pulsatile physiological waveform attributed to cardiac synchronous changes in the blood volume with each heart beat
 
 if __name__ == "__main__":
     # Load PPG data
-    timestamps, ppg_values = load_ppg_data("ppg_AR/green1_AR.csv")
+    timestamps, ppg_values = load_ppg_data("ppg_RED/red3_AR.csv")
 
     # Remove DC component
     ppg_ac = remove_dc_component(ppg_values)
