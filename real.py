97d186725675c285b5e7b8bee5dd532a9e5cec12
Aneek Roy <hawkroy22@gmail.com>
2024-04-09 10:48:47 -0500

    AR

--- real.py
+++ real.py
@@ -13,4 +13,12 @@ ppg_data = pd.read_csv(file)
 noisy_ppgOne = ppg_data["PPG1"].values
 
 
-# Step 1 : Grad Gain & DAC from csv file
+# Step 1 : Grab Gain & DAC from csv file
+
+
+# Step 2 : Input into Function
+
+# Guidelines
+
+# RAW1 = PPG1 / (gain1 + (dac1/ 5)*2^16 )
+# RAW2 = PPG2 / (gain2 + (dac2/ 5)*2^16 )
