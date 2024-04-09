dec0f0baa7fec74ac2bf02cb9cf3df2a60cd33cf
Aneek Roy <hawkroy22@gmail.com>
2024-04-09 11:08:31 -0500

    Ar

--- real.py
+++ real.py
@@ -8,12 +8,21 @@ from glob import glob
 import matplotlib.pyplot as plt
 
 
-
+file = "IF_1_gainCheck.csv"
 ppg_data = pd.read_csv(file)
-noisy_ppgOne = ppg_data["PPG1"].values
+
+ppgOne = ppg_data["PPG1"].values
+ppgTwo = ppg_data["PPG2"].values
 
 
 # Step 1 : Grab Gain & DAC from csv file
+
+gainOne = ppg_data["GAIN1"].values
+gainTwo = ppg_data["GAIN2"].values
+
+dacOne = ppg_data["DAC1"].values
+dacTwo = ppg_data["DAC2"].values
+
 
 
 # Step 2 : Input into Function
@@ -22,3 +31,14 @@ noisy_ppgOne = ppg_data["PPG1"].values
 
 # RAW1 = PPG1 / (gain1 + (dac1/ 5)*2^16 )
 # RAW2 = PPG2 / (gain2 + (dac2/ 5)*2^16 )
+
+def findRaw():
+    rawOne = []
+    rawTwo = []
+    for i,j in ppgOne, ppgTwo:
+        rawOne.append( i / (gainOne + (dacOne/ 5)*2^16 ))
+        rawTwo.append( j / (gainTwo + (dacTwo/ 5)*2^16 ))
+    print(rawOne)
+
+
+
